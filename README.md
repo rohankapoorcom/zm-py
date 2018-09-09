27ef6faa19cff2c4f8e5128d964e3433ac2af92b
Rohan Kapoor <rohan@rohankapoor.com>
2018-09-09 16:36:19 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+A loose python wrapper around the [ZoneMinder](www.zoneminder.org) API.
+
+Based on code that was originally part of [Home Assistant](www.home-assistant.io). Historical sources and authorship information is available as part of the Home Assistant project:
+* [ZoneMinder Platform](https://github.com/home-assistant/home-assistant/commits/dev/homeassistant/components/zoneminder.py)
+* [ZoneMinder Camera](https://github.com/home-assistant/home-assistant/commits/dev/homeassistant/components/camera/zoneminder.py)
+* [ZoneMinder Sensor](https://github.com/home-assistant/home-assistant/commits/dev/homeassistant/components/sensor/zoneminder.py)
+* [ZoneMinder Switch](https://github.com/home-assistant/home-assistant/commits/dev/homeassistant/components/switch/zoneminder.py)
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+long_description = open('README.md').read()
+
+setup(
+    name='zm-py',
+    version='0.0.1',
+    license='Apache Software License',
+    url='https://github.com/rohankapoorcom/zm-py',
+    author='Rohan Kapoor',
+    author_email='rohan@rohankapoor.com',
+    description='A loose python wrapper around the Zoneminder REST API.',
+    long_description=long_description,
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=list(val.strip() for val in open('requirements.txt')),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
