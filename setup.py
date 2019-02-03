ac70bafd7516d155d5c682d96b73b1335d04a962
Rohan Kapoor <rohan@rohankapoor.com>
2019-02-02 23:33:43 -0800

    Bump version to 0.3.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = open('README.md').read()
 
 setup(
     name='zm-py',
-    version='0.3.1',
+    version='0.3.2',
     license='Apache Software License',
     url='https://github.com/rohankapoorcom/zm-py',
     author='Rohan Kapoor',
